cd06fc73552b60f68e36a2490f178e5b62d8b9fd
ron-ligsay <akyligsay16@gmail.com>
2023-12-21 20:58:49 +0800

    cleaning out the recommender function

--- apps/recommender/views.py
+++ apps/recommender/views.py
@@ -20,6 +20,11 @@ import os
 import plotly.express as px
 import plotly.io as pio
 
+# import minmaxscaler
+from sklearn.preprocessing import MinMaxScaler
+# import tfidf
+from sklearn.feature_extraction.text import TfidfTransformer
+
 def load_csv(request):
     csv_file_name = 'field_skills_tfidf.csv'
     csv_file_path = os.path.join(os.path.dirname(__file__), csv_file_name)
@@ -27,46 +32,118 @@ def load_csv(request):
     try:
         # Read the CSV file into a DataFrame
         specialization_sparse = pd.read_csv(csv_file_path)
-
         # Process the DataFrame as needed (e.g., save to the database, perform operations)
-
         return specialization_sparse
     except Exception as e:
         #print("Error processing file: {}".format(e))
         # empty DataFrame with the same columns and dtypes as your original DataFrame
-        
         specialization_sparse = pd.DataFrame()
-
         return specialization_sparse
 
-# import minmaxscaler
-from sklearn.preprocessing import MinMaxScaler
 
-# import tfidf
-from sklearn.feature_extraction.text import TfidfTransformer
+
+def get_user_skills(request):
+    user_skills = UserSkill.objects.filter(user=request.user)
+    user_skills_list = [skill.skill.skill for skill in user_skills]
+    return list(set(user_skills_list))
+
+def get_intersection_columns(user_skills_list, normalized_field_skills):
+    normalized_field_skills_columns = list(normalized_field_skills.columns)
+    return list(set(user_skills_list).intersection(set(normalized_field_skills_columns)))
+
+def create_skill_plot(user_skills_df):
+    fig = px.bar(
+        user_skills_df,
+        x='level',
+        y='field',
+        barmode ='stack',
+        hover_data=['skill'],
+        orientation='h',
+        labels={'level': 'Count', 'field': 'Field'},
+        #color_discrete_sequence=list(field_dict.values()),
+        color='field',
+    )
+
+    # x and y title
+    #fig.update_xaxes(title_text='Skill Level')
+    #fig.update_yaxes(title_text='Skill Name')
+    fig.update_xaxes(title_text='Count')
+    fig.update_yaxes(title_text='Field')
+    skill_plot = pio.to_html(fig, full_html=False)
+    return skill_plot
+
+def create_field_plot(fields_df):
+     # Create a pie chart using Plotly Express
+    fig = px.pie(fields_df, values='Score', names='Field_Name',
+                 #title='Top Field Recommendation Score'
+                 )
+    # set title
+    #fig.update_layout(title_text='Top Field Recommendation Score', title_x=0.5)
+
+    # remove white background
+    fig.update_layout(plot_bgcolor='rgba(0,0,0,0)')
+    # Convert the figure to HTML
+    field_plot = pio.to_html(fig, full_html=False)
+    return field_plot
+
+
+def create_stacked_skills(normalized_copy):
+    stacked_skills = px.bar(
+        normalized_copy, 
+        x='level', 
+        y='field_name', 
+        color='field_name', 
+        # title='Skills Levels',
+        orientation='h',
+        labels={'level': 'Relevance Score',  'skill': 'Skill'}, #'field_name': 'Field Name',
+        color_continuous_scale=px.colors.sequential.Plasma,
+        height=500,
+        width=800,
+    )
+
+    # convert to html
+    stacked_skills = pio.to_html(stacked_skills, full_html=False)
+    return stacked_skills
+
+def create_radar_skills(normalized_copy):
+    # create the radar chart
+    radar_skills = px.line_polar(
+        normalized_copy, 
+        r='level', 
+        theta='skill', 
+        color='field_name', 
+        line_close=True,
+        # title='Skills Levels',
+        labels={'level': 'Relevance Score', 'field_name': 'Field Name'},
+        #color_continuous_scale=px.colors.sequential.Plasma,
+        height=500,
+        width=800,
+    )
+    radar_skills = pio.to_html(radar_skills, full_html=False)
+    return radar_skills
+
+def save_user_recommendation(request, top_3_fields, top_3_fields_score):
+        # get StudentProfile year
+    student_year = StudentProfile.objects.get(user=request.user).current_year
+
+    #UserRecommendations get or create
+    user_recommendations = UserRecommendations.objects.get_or_create(user=request.user, current_year=student_year)[0]
+
+    user_recommendations.field_1 = Field.objects.get(field=top_3_fields[0])
+    user_recommendations.field_2 = Field.objects.get(field=top_3_fields[1])
+    user_recommendations.field_3 = Field.objects.get(field=top_3_fields[2])
+    user_recommendations.score_1 = float(top_3_fields_score[0])
+    user_recommendations.score_2 = float(top_3_fields_score[1])
+    user_recommendations.score_3 = float(top_3_fields_score[2])
+
+
+    user_recommendations.save()
 
 def recommender(request):
     # Get the user's skills from the request.
-    user_skills = UserSkill.objects.filter(user=request.user)
-    user_skills_list = [skill.skill.skill for skill in user_skills]
-    user_skills_list = list(set(user_skills_list))
-    user_skills_list = list(user_skills_list)
-
+    user_skills_list = get_user_skills(request)
     normalized_field_skills = load_csv(request)
-
-    normalized_user_skills_df_columns = user_skills_list
-    
-    normalized_field_skills_columns = normalized_field_skills.columns
-    # convert to list
-    normalized_field_skills_columns = list(normalized_field_skills_columns)
-    #intersection_columns = normalized_user_skills_df_columns.intersection(normalized_field_skills_columns)
-    intersection_columns = set(normalized_user_skills_df_columns).intersection(set(normalized_field_skills_columns))
-
-
-    # to list for filtering
-    intersection_columns = list(intersection_columns)
-    print("Len of interseciotn columns: ", len(intersection_columns))
-    #print('intersection_columns: ', intersection_columns)
+    intersection_columns = get_intersection_columns(user_skills_list, normalized_field_skills)
 
     if len(intersection_columns) == 0:
         return render(request, 'recommender/recommender_failed.html')
@@ -94,7 +171,7 @@ def recommender(request):
     cosine_similarities = cosine_similarity(normalized_user_skills_df[intersection_columns], normalized_field_skills_filtered[intersection_columns])
     #print('cosine_similarities: ', cosine_similarities)
     top_3_indices = cosine_similarities.argsort(axis=1)[:, -3:]
-    top_3_field = normalized_field_skills_filtered.iloc[:, 0].values[top_3_indices]
+    top_3_field = normalized_field_skills_filtered.iloc[:, 0].values[top_3_indices] # rendered
     all_fields = normalized_field_skills_filtered.iloc[:, 0].values
  
     field_ids = normalized_field_skills_filtered['field_id'].unique()
@@ -110,6 +187,7 @@ def recommender(request):
         field_name = Field.objects.get(field=field_id).field_name
         fields_name.append(field_name)
 
+    # should be replace by website_field.id
     field_dict = {
         'Software Development': 1,
         'Data and Analytics': 2,
@@ -138,46 +216,17 @@ def recommender(request):
     })
     #pivot_df = user_skills_df.pivot(index='skill', columns='field', values='level').fillna(0)
 
-
     user_skills_df['fields_score'] = user_skills_df['field_id'].map(fields_score)
     user_skills_df = user_skills_df.sort_values('fields_score', ascending=False)
     user_skills_df = user_skills_df.drop('fields_score', axis=1)
 
-
-    fig = px.bar(
-        user_skills_df,
-        x='level',
-        y='field',
-        barmode ='stack',
-        hover_data=['skill'],
-        orientation='h',
-        labels={'level': 'Count', 'field': 'Field'},
-        #color_discrete_sequence=list(field_dict.values()),
-        color='field',
-    )
-
-    # x and y title
-    #fig.update_xaxes(title_text='Skill Level')
-    #fig.update_yaxes(title_text='Skill Name')
-    fig.update_xaxes(title_text='Count')
-    fig.update_yaxes(title_text='Field')
-    skill_plot = pio.to_html(fig, full_html=False)
-
+    skill_plot = create_skill_plot(user_skills_df)
+   
     # Create a DataFrame with Field_ID, Field_Name, and Score
-    fields_df = pd.DataFrame(list(zip(field_ids, fields_name, fields_score.values())), columns=['Field_ID', 'Field_Name', 'Score'])
-    print('fields_df: ', fields_df)
-    # Create a pie chart using Plotly Express
-    fig = px.pie(fields_df, values='Score', names='Field_Name',
-                 #title='Top Field Recommendation Score'
-                 )
+    fields_df = pd.DataFrame(list(zip(field_ids, fields_name, fields_score.values())), columns=['Field_ID', 'Field_Name', 'Score'])   
+    field_plot = create_field_plot(fields_df)
+   
 
-    # set title
-    #fig.update_layout(title_text='Top Field Recommendation Score', title_x=0.5)
-
-    # remove white background
-    fig.update_layout(plot_bgcolor='rgba(0,0,0,0)')
-    # Convert the figure to HTML
-    field_plot = pio.to_html(fig, full_html=False)
     # make a copy of normamlied_field
     normalized_copy = normalized_field_skills_filtered.copy()
     # label field id of normalized_field_skills_filtered with field_dict
@@ -191,38 +240,11 @@ def recommender(request):
     # remove field_order
     normalized_copy = normalized_copy.drop('field_order', axis=1)
 
-    print('normalized_copy: ', normalized_copy)
+    #print('normalized_copy: ', normalized_copy)
 
-    stacked_skills = px.bar(
-        normalized_copy, 
-        x='level', 
-        y='field_name', 
-        color='field_name', 
-        # title='Skills Levels',
-        orientation='h',
-        labels={'level': 'Relevance Score',  'skill': 'Skill'}, #'field_name': 'Field Name',
-        color_continuous_scale=px.colors.sequential.Plasma,
-        height=500,
-        width=800,
-    )
+    stacked_skills = create_stacked_skills(normalized_copy)
 
-    # convert to html
-    stacked_skills = pio.to_html(stacked_skills, full_html=False)
-
-    # create the radar chart
-    radar_skills = px.line_polar(
-        normalized_copy, 
-        r='level', 
-        theta='skill', 
-        color='field_name', 
-        line_close=True,
-        # title='Skills Levels',
-        labels={'level': 'Relevance Score', 'field_name': 'Field Name'},
-        #color_continuous_scale=px.colors.sequential.Plasma,
-        height=500,
-        width=800,
-    )
-    radar_skills = pio.to_html(radar_skills, full_html=False)
+    radar_skills = create_radar_skills(normalized_copy)
 
     # Sort the fields by the sum of the cosine similarity scores, in descending order.
     top_3_fields = sorted(fields_score, key=fields_score.get, reverse=True)[:3]
@@ -237,38 +259,60 @@ def recommender(request):
     field_name_2 = field_names[1]
     field_name_3 = field_names[2]
 
-    # get StudentProfile year
-    student_year = StudentProfile.objects.get(user=request.user).current_year
+    # saving user recommendation
+    save_user_recommendation(request, top_3_fields, top_3_fields_score)
 
-    #UserRecommendations get or create
-    user_recommendations = UserRecommendations.objects.get_or_create(user=request.user, current_year=student_year)[0]
-
-    user_recommendations.field_1 = Field.objects.get(field=top_3_fields[0])
-    user_recommendations.field_2 = Field.objects.get(field=top_3_fields[1])
-    user_recommendations.field_3 = Field.objects.get(field=top_3_fields[2])
-    user_recommendations.score_1 = float(top_3_fields_score[0])
-    user_recommendations.score_2 = float(top_3_fields_score[1])
-    user_recommendations.score_3 = float(top_3_fields_score[2])
-
-
-    user_recommendations.save()
-
+    # Roadmap
+    user_reco_step_1 = UserRecommendations.objects.filter(user=request.user, current_year=0).exists()
+    user_reco_step_2 = UserRecommendations.objects.filter(user=request.user, current_year=1).exists()
+    user_reco_step_3 = UserRecommendations.objects.filter(user=request.user, current_year=2).exists()
+    user_reco_step_4 = UserRecommendations.objects.filter(user=request.user, current_year=3).exists()
+    user_reco_step_5 = UserRecommendations.objects.filter(user=request.user, current_year=4).exists()
 
     return render(request, 'recommender/recommender.html', {
+        # Top Results
         'top_3_field_recommendations': top_3_field,
         'field_name_1': field_name_1,
         'field_name_2': field_name_2,
         'field_name_3': field_name_3,
-        'skill_plot': skill_plot,
-        'field_plot': field_plot,
-        'stacked_skills': stacked_skills,
-        'radar_skills': radar_skills,
         'field_1': Field.objects.get(field=top_3_fields[0]),
         'field_2': Field.objects.get(field=top_3_fields[1]),
         'field_3': Field.objects.get(field=top_3_fields[2]),
+        
+        # Plots
+        'skill_plot': skill_plot,           # skill score representation
+        'field_plot': field_plot,           # pie graph field recommendation
+        'stacked_skills': stacked_skills,   # skill relevance representation
+        #'radar_skills': radar_skills,      # skill relevance representation
+
+        # Roadmap
+        'step_1': user_reco_step_1,
+        'step_2': user_reco_step_2,
+        'step_3': user_reco_step_3,
+        'step_4': user_reco_step_4,
+        'step_5': user_reco_step_5,
+
+
     })
 
 
+def roadmap(request):
+     # Roadmap
+    user_reco_step_1 = UserRecommendations.objects.filter(user=request.user, current_year=0).exists()
+    user_reco_step_2 = UserRecommendations.objects.filter(user=request.user, current_year=1).exists()
+    user_reco_step_3 = UserRecommendations.objects.filter(user=request.user, current_year=2).exists()
+    user_reco_step_4 = UserRecommendations.objects.filter(user=request.user, current_year=3).exists()
+    user_reco_step_5 = UserRecommendations.objects.filter(user=request.user, current_year=4).exists()
+
+    return render(request, 'recommender/roadmap.html', {
+        # Roadmap
+        'step_1': user_reco_step_1,
+        'step_2': user_reco_step_2,
+        'step_3': user_reco_step_3,
+        'step_4': user_reco_step_4,
+        'step_5': user_reco_step_5,
+    })
+    
 
 def recommendation_field(request, field_id):
     
