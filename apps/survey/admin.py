1f34138e69809fbc2a8d44147793096a632603c4
Kristof <krstfpmbd20@gmail.com>
2023-12-28 14:00:33 +0800

    removed survey app

--- apps/survey/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Survey
-
-class SurveyAdmin(admin.ModelAdmin):
-    list_display = ('user', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', )
-    search_fields = ('q1', 'q2', 'q3', 'q5', 'q6', 'user')  # Add any other fields you want to search on
-
-admin.site.register(Survey, SurveyAdmin)
--- apps/survey/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.survey.views import survey, thank_you
-
-urlpatterns = [
-    path('survey/', survey, name='survey'),
-    path('thank_you/', thank_you, name='thank_you'),
-
-]
