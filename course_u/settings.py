75817fbe0dff93a4e0c8ddbad02da828f0de1790
Umair Choudhary <82431716+umair-choudhary94@users.noreply.github.com>
2024-01-09 17:30:46 +0500

    Update settings.py

--- course_u/settings.py
+++ course_u/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 INTERNAL_IPS= [
     '127.0.0.1',
 ]
-CSRF_TRUSTED_ORIGINS = ["https://courseu-production-d2b3.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://www.courseu.site"]
 WSGI_APPLICATION = 'course_u.wsgi.application'
 
 
