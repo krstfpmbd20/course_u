5d9c7191a993d46c57f48195f4f32a02cc7d07d4
ron-ligsay <akyligsay16@gmail.com>
2024-01-13 13:53:53 +0800

    url for reco survey validation

--- apps/recommender_survey/urls.py
+++ apps/recommender_survey/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.recommender_survey.views import survey, thank_you
+from apps.recommender_survey.views import survey, thank_you, survey_validation
 
 urlpatterns = [
+    path('recommender_survey_validation/', survey_validation, name='recommender_survey_validation'),
     path('recommendersurvey/', survey, name='recommender_survey'),
     path('recommender_thank_you/', thank_you, name='recommender_thank_you'),
 ]
